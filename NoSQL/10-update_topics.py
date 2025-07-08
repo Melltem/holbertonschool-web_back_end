a981c59814dd3a01e3283389eac540bd15743ee6
Melltem <meltemqasimli@gmail.com>
2025-07-08 11:44:12 +0000

    10. Change school topics

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+""" comment. """
+
+
+def update_topics(mongo_collection, name, topics):
+    """ comment. """
+    mongo_collection.update_many(
+        { "name": name },
+        { "$set": {"topics": topics } }
+)
