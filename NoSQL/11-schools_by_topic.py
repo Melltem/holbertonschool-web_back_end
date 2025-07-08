175c762c246fdf1697f81de115466e475ce31fdb
Melltem <meltemqasimli@gmail.com>
2025-07-08 11:58:07 +0000

    11. Where can I learn Python?

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -5,6 +5,4 @@
 
 def schools_by_topic(mongo_collection, topic):
     """ comment. """
-    return list(mongo_collection.find(
-            { "topic": topic }
-            ))
+    return list(mongo_collection.find({ "topics": topic }))
